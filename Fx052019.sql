ff793b49fed0249cc9333e606735c635839ea318
Stanley Williamson <stanleywilliamson47@gmail.com>
2021-01-24 20:09:23 -0500

    Add project files.

---
+++ Fx052019.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Fx052019]
+(
+	ticket float,
+	open_time datetime,
+	order_type varchar(255),
+	size decimal,
+	pair varchar(255),
+	stop_loss decimal,
+	take_profit decimal,
+	close_time datetime,
+	price decimal,
+	profit FLOAT
+)
